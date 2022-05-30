728131817348bd70e9c012c5a683b5b9859b6535
Edes0 <90324120+Edes0@users.noreply.github.com>
2022-05-30 16:47:34 +0200

    17.0 Done

--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -6,12 +6,18 @@ namespace Service.Contracts
 {
     public interface IEmployeeService
     {
-        Task<IEnumerable<EmployeeDto>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges);
-        Task<EmployeeDto> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges);
-        Task<EmployeeDto> CreateEmployeeForCompanyAsync(Guid companyId, EmployeeForCreationDto employeeForCreation, bool trackChanges);
-        Task DeleteEmployeeForCompanyAsync(Guid companyId, Guid id, bool trackChanges);
-        Task UpdateEmployeeForCompanyAsync(Guid companyId, Guid id, EmployeeForUpdateDto employeeForUpdate, bool compTrackChanges, bool empTrackChanges);
-        Task<(EmployeeForUpdateDto employeeToPatch, Employee employeeEntity)> GetEmployeeForPatchAsync(Guid companyId, Guid id, bool compTrackChanges, bool empTrackChanges);
+        Task<(IEnumerable<EmployeeDto> employees, MetaData metaData)> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, 
+            bool trackChanges);
+        Task<EmployeeDto> GetEmployeeAsync(Guid companyId, Guid id, 
+            bool trackChanges);
+        Task<EmployeeDto> CreateEmployeeForCompanyAsync(Guid companyId, EmployeeForCreationDto employeeForCreation, 
+            bool trackChanges);
+        Task DeleteEmployeeForCompanyAsync(Guid companyId, Guid id, 
+            bool trackChanges);
+        Task UpdateEmployeeForCompanyAsync(Guid companyId, Guid id, EmployeeForUpdateDto employeeForUpdate, bool compTrackChanges, 
+            bool empTrackChanges);
+        Task<(EmployeeForUpdateDto employeeToPatch, Employee employeeEntity)> GetEmployeeForPatchAsync(Guid companyId, Guid id, bool compTrackChanges,
+            bool empTrackChanges);
         Task SaveChangesForPatchAsync(EmployeeForUpdateDto employeeToPatch, Employee employeeEntity);
     }
 }
